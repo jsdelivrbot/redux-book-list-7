af685295a3f314d2245b298b599cc1b0a01a5316
Paulo Matos <paulojomatos@gmail.com>
2017-04-29 00:32:56 -0400

    Finished book list application

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (state, action) {
+    return [
+        { title: 'JavaScript: The Good Parts', pages: 131  },
+        { title: 'Harry Potter', pages: 34 },
+        { title: 'The Dark Tower', pages: 145 },
+        { title: 'Eloquent Ruby', pages: 543 },
+    ]
+}
